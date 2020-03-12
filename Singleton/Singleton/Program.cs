3e2aa01d276756d73b15246c39fdc72f63419fe6
JeremiAc <60486520+JeremiAc@users.noreply.github.com>
2020-03-11 23:43:24 -0400

    Add files via upload

---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Singleton.Instance.enviar);
+
+            Singleton.Instance.enviar = "Bienvenid@ Usuario";
+
+            Console.WriteLine(Singleton.Instance.enviar);
+
+        }
+    }
+}
---
+++ Singleton/Singleton/Singleton.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Singleton
+    
+{
+    public class Singleton
+    {
+        private static Singleton estructura = null;
+
+            public string enviar = "";
+        protected Singleton() 
+        {
+            enviar = "Bienvenid@";
+        }
+
+        public static Singleton Instance
+            
+        {
+            get
+            {
+                if (estructura == null)
+                    estructura = new Singleton();
+
+                return estructura;
+            }
+        }
+    }
+}
